4c8815f70abbce4fefd312c18d42420cd988822f
Saba <34461238+sbbzplt@users.noreply.github.com>
2022-11-19 18:13:05 +0300

    Create celestials.py

---
+++ celestials.py
@@ -0,0 +1,50 @@
+class Satellite:
+    def __init__(self, name, mass, radius, gravitational_acceleration):
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.gravitational_acceleration = gravitational_acceleration
+
+        
+class Planet:
+    def __init__(self, name, mass, radius, gravitational_acceleration, aphelion, perihelion, eccentricity, mean_density):
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.gravitational_acceleration = gravitational_acceleration
+        self.aphelion = aphelion
+        self.perihelion = perihelion
+        self.eccentricity = eccentricity
+        self.mean_density = mean_density
+        
+        
+class Star:
+    def __init__(self, name, mass, radius, gravitational_acceleration, mean_density, luminosity, temperature, metallicity, spectral_type, distance):
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.gravitational_acceleration = gravitational_acceleration
+        self.mean_density = mean_density
+        self. luminosity = luminosity
+        self.temperature = temperature
+        self.metallicity = metallicity
+        self.spectral_type = spectral_type
+        self.distance = distance
+
+        
+class RedGiant:
+    def __init__(self, name, mass, radius, gravitational_acceleration, mean_density, luminosity, temperature, distance):
+    
+    
+class NeutronStar:
+    def __init__(self, name, mass, radius, gravitational_acceleration, spectral_type, period, eccentricity, distance):
+
+        
+class WhiteDwarf:
+    def __init__(self, name, mass, radius, gravitational_acceleration, distance):
+        
+        
+class Blackhole:
+    def __init__(self, name, mass, radius, gravitational_acceleration, spin, charge, distance):
+        
+              
